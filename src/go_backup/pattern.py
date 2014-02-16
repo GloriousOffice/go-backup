095a11c8758d308603f38cfc7221322808ea163f
Ludwig Schmidt <ludwigschmidt2@gmail.com>
2014-02-16 17:09:19 -0500

    work on pattern matching: assemble_filenames

--- src/go_backup/pattern.py
+++ src/go_backup/pattern.py
@@ -27,6 +27,28 @@ def parse_pattern_file(f):
 def filename_matches_pattern(filename, pattern):
 
 
-def filename_matches_patterns(filename, patterns):
+def pattern_decision(filename, patterns, default_decision=INCLUDE):
+    res = default_decision
     for p in patterns:
-        
+        if filename_matches_single_pattern(filename, p[1]):
+            res = p[0]
+    return res
+
+def listdir_onerror(error):
+    raise error
+
+def assemble_filenames(rootdir, patterns):
+    res = []
+    for root, dirs, files in os.walk(rootdir, topdown=True,
+            onerror=listdir_onerror, followlinks=False)
+        for f in files:
+            filename = os.path.join(root, f)
+            # The "root" returned by os.walk includes rootdir. We strip out this
+            # part so that the filename starts with os.sep.
+            filename = 
+            decision = pattern_decision(filename, patterns)
+            if decision == INCLUDE:
+                res.append(filename)
+            elif decision != EXCLUDE:
+                raise ValueError('Unknown file decision {}.'.format(decision))
+    return res
